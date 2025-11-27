52139960f40409856e9254ed9dd4ca3284193ac2
Sun Yi an <158817429+qza666@users.noreply.github.com>
2025-11-28 07:16:36 +0900

    Fix nullable previous in custom exceptions

--- app/Exceptions/AppException.php
+++ app/Exceptions/AppException.php
@@ -7,7 +7,7 @@ namespace App\Exceptions;
 class AppException extends \Exception
 {
 
-    public function __construct($message = "", $code = 0, \Throwable $previous = null)
+    public function __construct($message = "", $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
